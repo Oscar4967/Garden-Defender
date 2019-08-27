ed48a5ffbea8dd55690c536d0aa6c3ce3cddf08f
Oscar <35801674+Oscar4967@users.noreply.github.com>
2019-08-27 21:00:07 +0900

    Start/Splash Screen
    Added start screen and splash screen with a sound effect

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    // Script for making the "loading screen" last for the duration of the sound byte
+
+    float durationOfSound = 1f;
+    int currentSceneIndex;
+
+
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if(currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitForSound());
+        }
+    }
+
+    IEnumerator WaitForSound()
+    {
+        yield return new WaitForSeconds(durationOfSound);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
